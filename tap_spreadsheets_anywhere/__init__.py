e92a8ad43cfcb91ad8b71b418162bc460275b07c
petrikoro <itsme@petrikoro.ru>
2024-06-13 13:47:18 +0300

    Update __init__.py

--- tap_spreadsheets_anywhere/__init__.py
+++ tap_spreadsheets_anywhere/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import json
 import logging
 
 import dateutil
@@ -26,7 +27,7 @@ def append_env_to_tables(table_spec):
     config['tables'][0]['pattern'] = os.environ.get('TAP_SPREADSHEET_ANYWHERE_PATTERN', config['tables'][0]['pattern'])
     config['tables'][0]['quotechar'] = os.environ.get('TAP_SPREADSHEET_ANYWHERE_QUOTECHAR', config['tables'][0]['quotechar'])
     config['tables'][0]['start_date'] = os.environ.get('TAP_SPREADSHEET_ANYWHERE_START_DATE', config['tables'][0]['start_date'])
-    config['tables'][0]['key_properties'] = os.environ.get('TAP_SPREADSHEET_ANYWHERE_KEY_PROPERTIES', [])
+    config['tables'][0]['key_properties'] = json.loads(os.environ.get('TAP_SPREADSHEET_ANYWHERE_KEY_PROPERTIES', "[]"))
 
     return config
 
